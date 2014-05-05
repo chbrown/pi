a3661eccf1c6f0105e34a0ee24328022bf4e6b92
Christopher Brown <io@henrian.com>
2014-05-05 09:40:42 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filepath):
 
 setup(
     name='pi',
-    version='0.1.1',
+    version='0.1.2',
     description='Simpler python package installation',
     url='https://github.com/chbrown/pi',
     author='Christopher Brown',
